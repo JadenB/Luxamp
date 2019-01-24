ee10f58ac86be07050e17b50c1b4dc2fef53acb6
JadenB <jadenbbernal@gmail.com>
2019-01-23 21:14:28 -0800

    Version 0.1.1

---
+++ Carthage/Checkouts/GistSwift/GistSwift/GistSwift.h
@@ -0,0 +1,14 @@
+/////    ___ _    _   ___        _  __ _
+////    / __(_)__| |_/ __|_ __ _(_)/ _| |_
+///    | (_ | (_-<  _\__ \ V  V / |  _|  _|
+//      \___|_/__/\__|___/\_/\_/|_|_|  \__|
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GistSwift.
+FOUNDATION_EXPORT double GistSwiftVersionNumber;
+
+//! Project version string for GistSwift.
+FOUNDATION_EXPORT const unsigned char GistSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GistSwift/PublicHeader.h>
